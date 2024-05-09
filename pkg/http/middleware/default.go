8ce05a2e111351a33f2549ae09423d6aff9dafd0
Liam Symonds <47109131+lsymds@users.noreply.github.com>
2024-05-09 21:52:04 +0100

    feat: Add middleware HTTP package (#1)
    * add initial middleware
    * add tests
    * update ci

---
+++ pkg/http/middleware/default.go
@@ -0,0 +1,41 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+// Logging is a middleware that adds a logger enriched with details of the HTTP request to the request's context.
+func Logging(h http.Handler, enricher func(c *zerolog.Context)) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		l := log.With().
+			Str("url", r.URL.String()).
+			Str("method", r.Method)
+
+		if enricher != nil {
+			enricher(&l)
+		}
+
+		r = r.WithContext(l.Logger().WithContext(r.Context()))
+
+		h.ServeHTTP(w, r)
+	})
+}
+
+// Recovery recovers any panics, logs an error and executes the provided action.
+func Recovery(h http.Handler, onPanic http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		l := zerolog.Ctx(r.Context())
+
+		defer func() {
+			if err := recover(); err != nil {
+				l.Error().Any("panic", err).Msg("recovered from panic")
+				onPanic.ServeHTTP(w, r)
+			}
+		}()
+
+		h.ServeHTTP(w, r)
+	})
+}
---
+++ pkg/http/middleware/middleware.go
@@ -0,0 +1,3 @@
+// Package middleware contains numerous HTTP middlewares for day to day operations. Where possible, it uses the
+// Go standard library, but it will occassionally pull in other packages where required.
+package middleware
